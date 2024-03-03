cb3183193f5e20831ec8d0ade8bdb79591754b8f
vivekchaubey <vivek.chaubey@iitdalumni.com>
2024-03-02 19:11:01 -0500

    reading from google sheet

---
+++ Random Number Guess/read_from_google_sheet.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+sheet_id = '1nq1YQhUMDLvlMx4Oo4NGbod0l-XBzULronWhau9CUl0'
+df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+
+print(df)
