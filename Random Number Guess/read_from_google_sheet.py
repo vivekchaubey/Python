9a2a6660e5a38e4df8ae87b6171dc499d9fa76f7
vivekchaubey <vivek.chaubey@iitdalumni.com>
2024-03-02 19:48:36 -0500

    print

--- Random Number Guess/read_from_google_sheet.py
+++ Random Number Guess/read_from_google_sheet.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 sheet_id = '1nq1YQhUMDLvlMx4Oo4NGbod0l-XBzULronWhau9CUl0'
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
-print("printing")
+print("printing dataframe")
 print(df)
